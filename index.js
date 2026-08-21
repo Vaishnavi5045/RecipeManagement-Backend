ae88bcdad9904309132527a6b5bc17af0a8706ef
vaishnavi <vaishnavigadge2012@gmail.com>
2026-08-21 17:17:17 +0530

    project changes

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const mysql2 = require("mysql2");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
