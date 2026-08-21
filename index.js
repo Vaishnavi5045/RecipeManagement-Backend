e5b75a9b1e7055c04596ec8dacac58169722d595
vaishnavi <vaishnavigadge2012@gmail.com>
2026-08-21 17:11:15 +0530

    project changes

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Backend server is running on port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend server is running on port ${PORT}`);
+});
